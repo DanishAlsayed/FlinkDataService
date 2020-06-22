1630afac6af47cf09f22ecfe363773ec2acdfaff
Danish Alsayed <danish.alsayed@gmail.com>
2020-06-22 22:40:24 +0800

    changed test library to jupiter

--- src/AcyclicJoinUnderUpdates/src/test/java/RelationStructureTest.java
+++ src/AcyclicJoinUnderUpdates/src/test/java/RelationStructureTest.java
@@ -1,17 +1,17 @@
 package src.test.java;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import src.main.java.RelationStructure;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static src.main.java.RelationStructure.*;
 
 public class RelationStructureTest {
     @Test
     public void getStructure() {
-        assertEquals(RelationStructure.getStructure(true, true),ONLY);
-        assertEquals(RelationStructure.getStructure(true, false),ROOT);
-        assertEquals(RelationStructure.getStructure(false, true),LEAF);
-        assertEquals(RelationStructure.getStructure(false, false),INTERMEDIATE);
+        assertEquals(RelationStructure.getStructure(true, true), ONLY);
+        assertEquals(RelationStructure.getStructure(true, false), ROOT);
+        assertEquals(RelationStructure.getStructure(false, true), LEAF);
+        assertEquals(RelationStructure.getStructure(false, false), INTERMEDIATE);
     }
 }
